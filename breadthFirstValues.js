d76edb873ce170abb15b0564a54754ee9d193977
niv suroghon <nivsuroghon@nivs-MBP.lan>
2023-01-23 11:34:16 -0500

    treeSum functions finds sum of all values in a binary tree

--- breadthFirstValues.js
+++ breadthFirstValues.js
@@ -51,16 +51,55 @@ const treeIncludesRecursive = (root, target) => {
     return treeIncludesRecursive(root.left, target) || treeIncludes(root.right, target);
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
+// compute total sum of all values in a tree using breadth first alg
+// breadth first - iterative
+const treeSum = (root) => {
+    if(root === null) return 0;
+
+    let sum = 0;
+    const queue = [ root ];
+
+    while(queue.length > 0) {
+        const current = queue.shift();
+        console.log(current.val)
+        sum += current.val;
+
+        if(current.left) queue.push(current.left)
+        if(current.right) queue.push(current.right)
+    }
+
+    return sum;
+}
+
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f
+
+// console.log(treeIncludes(a, "e"))
+
+const a = new Node(1);
+const b = new Node(6);
+const c = new Node(0);
+const d = new Node(3);
+const e = new Node(-6);
+const f = new Node(2);
+const g = new Node(2);
+const h = new Node(2);
+
 a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
-c.right = f
+c.right = f;
+e.left = g;
+f.right = h;
 
-console.log(treeIncludes(a, "e"))
+console.log(treeSum(a))
